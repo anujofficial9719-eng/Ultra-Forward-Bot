6e731770d2e957299d4ea799f23c4ea4ad63aac2
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-30 04:01:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "34724970")
     API_HASH = os.environ.get("API_HASH", "f240eae7c60e8e30c17203ab0e052f7e")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8656231605:AAGVZvUNt7VJiqq1r7MT_wbhCLO229ztU2I") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8747283809:AAG8fwnXeTt77duF_viYA6FbrmhzvWUo1U8") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "Anujedit")
